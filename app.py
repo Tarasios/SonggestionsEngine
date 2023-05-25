b9cf8476151b439a91f60f30d1db61700eaff829
Tarasios <cassiel@glennwilliams.ca>
2023-05-25 07:15:47 -0700

    Converted app.py to API format

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify
 import pandas as pd
 import tensorflow as tf
-
-# Import the necessary functions from process.py
 from process import load_songs, preprocess_text, build_song_model, define_task, create_retrieval_model, \
     setup_retrieval_model, get_song_recommendations
 
@@ -15,6 +13,7 @@ song_model = build_song_model()
 task = define_task(songs, song_model)
 model = create_retrieval_model(song_model, task)
 index = setup_retrieval_model(model, songs)
+
 # Load the data into the 'data' DataFrame
 data = pd.read_csv('Untrimmeddata.csv')
 try:
@@ -27,19 +26,22 @@ except OSError as e:
 def hello():
     return "Hello, Flask!"
 
-
-@app.route('/recommend/<int:song_id>')
+@app.route('/recommend/<int:song_id>', methods=['GET'])
 def recommend_song(song_id):
-    track = data[data['id'] == song_id]['Track'].values[0]  # Retrieve the associated track
-    recommendations, urls = get_song_recommendations(index, song_id, data)
+    try:
+        track = data[data['id'] == song_id]['Track'].values[0]  # Retrieve the associated track
+        recommendations, urls = get_song_recommendations(index, song_id, data)
 
-    response = {
-        'Given Song ID': song_id,
-        'Given song name': track,
-        'recommendations': recommendations.tolist(),
-        'urls': urls
-    }
-    return jsonify(response)
+        response = {
+            'Input Song ID': song_id,
+            'Input Song Name': track,
+            'Output Song ID': recommendations.tolist(),
+            'Output Song URLs': urls
+        }
+        return jsonify(response), 200  # Return a successful response
+    except Exception as e:
+        error_message = f"An error occurred: {str(e)}"
+        return jsonify({'error': error_message}), 500  # Return an error response
 
 
 if __name__ == '__main__':
